62c64431aff9368c10076296f0396d8bc048a872
Alex <alexscripkin2003@yandex.ru>
2020-04-12 18:09:24 +0300

    Добавил демо главной страницы.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,16 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 __factory = None
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'main'
+    data = [{'author_name': 'AuthorName1', 'author_img': '../../static/img/img2.jpg', 'date': '01.01.2020',
+             'note': 'NoteAboveMeme', 'meme_img': '../../static/img/img1.jpg', 'likes': 123, 'reposts': 321,
+             'is_liked': True, 'is_reposted': True},
+            {'author_name': 'AuthorName2', 'author_img': '../../static/img/img1.jpg', 'date': '02.02.2022',
+             'note': 'NoteAboveMeme', 'meme_img': '../../static/img/img2.jpg', 'likes': 123, 'reposts': 321,
+             'is_liked': True, 'is_reposted': True}
+            ]
+    return render_template('main.html', data=data)
 
 
 if __name__ == '__main__':
