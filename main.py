f15bec010e8db30c1ba4f1a6f29699c97c4d8002
Alex <alexscripkin2003@yandex.ru>
2020-04-21 16:41:00 +0300

    Практически доделал главную страницу

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask import Flask
 from data import db_session
 
 app = Flask(__name__)
@@ -8,16 +7,41 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/memehub.sqlite")
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = [{'author_name': 'AuthorName1', 'author_img': '../../static/img/img2.jpg', 'date': '01.01.2020',
-             'note': 'NoteAboveMeme', 'meme_img': '../../static/img/img1.jpg', 'likes': 123, 'reposts': 321,
-             'is_liked': True, 'is_reposted': True},
-            {'author_name': 'AuthorName2', 'author_img': '../../static/img/img1.jpg', 'date': '02.02.2022',
-             'note': 'NoteAboveMeme', 'meme_img': '../../static/img/img2.jpg', 'likes': 123, 'reposts': 321,
-             'is_liked': True, 'is_reposted': True}
-            ]
+    data = {'info': {'is_auth': True, 'user_img': '../../static/img/img1.jpg', 'username': 'User'},
+            'content': [
+                {'type': 'meme', 'id': '1', 'author_name': 'AuthorName1', 'author_img': '../../static/img/img2.jpg',
+                 'date': '01.01.2020',
+                 'note': '', 'meme_img': '../../static/img/img1.jpg', 'likes': 3674,
+                 'reposts': 25,
+                 'is_liked': False, 'is_reposted': True, 'category': 'category1', 'place': 13, 'delete': False},
+                {'type': 'meme', 'id': '2', 'author_name': 'AuthorName2', 'author_img': '../../static/img/img1.jpg',
+                 'date': '02.02.2022',
+                 'note': 'NoteAboveMeme2', 'meme_img': '../../static/img/img2.jpg', 'likes': 277,
+                 'reposts': 178,
+                 'is_liked': True, 'is_reposted': False, 'category': 'category2', 'place': 0, 'delete': True},
+                {'type': 'repost', 'id': '3', 'delete': True, 'author_name': 'AuthorName3',
+                 'author_img': '../../static/img/img1.jpg',
+                 'date': '02.02.2022', 'reposted_content': {'id': '2', 'author_name': 'AuthorName2',
+                                                            'author_img': '../../static/img/img1.jpg',
+                                                            'date': '02.02.2022',
+                                                            'note': 'NoteAboveMeme2',
+                                                            'meme_img': '../../static/img/img2.jpg',
+                                                            'likes': 277,
+                                                            'reposts': 178,
+                                                            'is_liked': True, 'is_reposted': False,
+                                                            'category': 'category2',
+                                                            'place': 0, 'delete': False}}]
+            }
     return render_template('main.html', data=data)
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    if request:
+        print(request.json)
+    return 'return'
 
 
 if __name__ == '__main__':
