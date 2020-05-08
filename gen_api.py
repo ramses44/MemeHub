fb96cf644d5bcaa5347019478d07c5e40281a269
Alex <alexscripkin2003@yandex.ru>
2020-05-08 11:14:00 +0300

    Исправил ошибку с отображением тегов

--- gen_api.py
+++ gen_api.py
@@ -53,7 +53,7 @@ def get_content(uid=0, k=0, data=()):
             'reposts': len(list(meme.repostes)),
             'is_liked': uid in map(lambda x: x.id, meme.likes),
             'is_reposted': uid in map(lambda x: x.user, meme.repostes),  # заменил x.id на x.user
-            'category': ", ".join(map(lambda x: x.title, meme.tags)),
+            'category': list(map(lambda x: x.title, meme.tags)),  # ", ".join(map(lambda x: x.title, meme.tags)),
             'place': meme_selector.get_most_popular().index(meme) + 1,
             'delete': uid == meme.author_.id or ses.query(User).get(uid).role != ROLES.index('user')
         }
